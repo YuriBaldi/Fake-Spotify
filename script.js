da4adcb9ea2d843815b493bf6a7143a00564427f
Yuri Baldi <yuri.baldi@unifesp.br>
2024-04-02 17:16:27 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const botaoPlayPause = document.getElementById("play-pause");
+const botaoProximo = document.getElementById("proximo");
+const botaoAnterior = document.getElementById("anterior");
+const nomeMusica = document.getElementById("musica");
+const audio = document.getElementById("audio");
+isPlaying = false;
+let musica = 1;
+const quantidadeMusicas =3;
+
+function tocarFaixa(){
+    audio.play();
+    botaoPlayPause.classList.remove("bi-play-circle-fill");
+    botaoPlayPause.classList.add("bi-pause-circle-fill");
+    isPlaying = true;
+}
+
+function pausarFaixa(){
+    audio.pause();
+    botaoPlayPause.classList.add("bi-play-circle-fill");
+    botaoPlayPause.classList.remove("bi-pause-circle-fill");
+    isPlaying = false;
+}
+
+function tocarOuPausarFaixa(){
+    if (isPlaying === true){
+        pausarFaixa();
+    }
+    else{
+        tocarFaixa();
+    }
+}
+
+function proximaFaixa(){
+    if (musica < quantidadeMusicas){
+        musica+= 1;
+    }else{
+        musica = 1;
+    }
+    audio.src = "./audio/" + musica + ".mp3";
+    nomeMusica.innerText = "Música " + musica;
+    tocarFaixa();
+    
+}
+
+function musicaAnterior(){
+    if (musica === 1){
+        musica = quantidadeMusicas;
+    } else{
+        musica-=1;
+    }
+    audio.src = "./audio/" + musica + ".mp3";
+    nomeMusica.innerText = "Música " + musica;
+    tocarFaixa();
+}
+
+botaoPlayPause.addEventListener('click', tocarOuPausarFaixa);
+botaoProximo.addEventListener('click', proximaFaixa);
+botaoAnterior.addEventListener('click', musicaAnterior);
+audio.addEventListener('ended', proximaFaixa);
